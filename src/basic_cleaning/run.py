c88937191583932a93b2e271dda90db795cd7c38
Alicia Higgs <aliciashiggs@gmail.com>
2026-09-20 14:50:57 -0700

    Fix data cleaning boundaries for out-of-bounds coordinates

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -2,6 +2,7 @@
 """
 Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
 """
+
 import argparse
 import logging
 import wandb
@@ -35,6 +36,8 @@ def go(args):
     # in the project.
     # Add longitude and latitude filter to allow test_proper_boundaries to pass
     # ENTER CODE HERE
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     # Save the cleaned data
     df.to_csv('clean_sample.csv',index=False)
